2015ea8df0ff5e68d09975f257107c69fca664d1
sinster2003 <sindhurvshabaraya2318@gmail.com>
2024-01-24 18:50:13 +0530

    typo-fix

--- frontend/src/utils/features/deletePost.js
+++ frontend/src/utils/features/deletePost.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const handleDeletePost = async (postId, username, navigate, toast, setUserLoggedIn) => {
     try {
-      if(!window.confirm("Are you want to delete this post?")) return;
+      if(!window.confirm("Are you sure? Do you want to delete this post?")) return;
       const response = await axios.delete(`/api/posts/delete/${postId}`, { withCredentials: true });
       const result = await response.data;
       if(result?.error) {
